60a3db2361eee438ecb7a469e11b6f35cfd67281
yang-wei <yangweilim21@gmail.com>
2016-02-14 15:33:00 +0900

    Navbar

--- src/Main.elm
+++ src/Main.elm
@@ -2,8 +2,8 @@ import Router exposing (match, Route, (:->))
 
 import Signal.Extra as Extra
 
-import Graphics.Element as Element exposing (Element, flow, left)
-import Html exposing (button, text, Html, div, a, ul, li, h1)
+import Graphics.Element as Element exposing (Element, flow, right)
+import Html exposing (button, text, Html, div, a, ul, li, h1, toElement)
 import Html.Attributes exposing (href)
 import History          exposing (hash, setPath)
 import Debug
@@ -16,19 +16,41 @@ navbar : Html
 navbar = 
         div [] 
         [ ul []
-             [ li [] [ a [href "#" ] [text "page-1" ] ] 
-             , li [] [ a [href "#/page2" ] [text "page-2" ] ] 
-             , li [] [ a [href "#/page3" ] [text "page-3" ] ] 
+             [ li [] [ a [href "#/basicSignal" ] [text "Basic Signals" ] ] 
+             , li [] [ a [href "#/timeSignal" ] [text "Time Signals" ] ] 
              ]
         ]
+
+signalNavbar : Signal Element
+signalNavbar = 
+  Signal.constant (toElement 150 50 navbar)
+
+routingElement = Signal.map routing signals
+
+main =
+  Extra.mapMany (flow right)
+  [ signalNavbar
+  , routingElement
+  ]
 
 signals : Signal (String, Element, Element)
 signals = Signal.map3 (,,) 
           (Signal.map (Debug.log "hash") hash)
           BasicSignal.app
           BasicSignal2.app
+ 
+routing (pagePath, basicSignal, timeSignal) =
+  let
+    allPage =
+      match [ "" :-> always basicSignal
+            , "#/basicSignal" :-> always basicSignal
+            , "#/timeSignal" :-> always timeSignal
+      ] (always basicSignal)
+  in
+    allPage pagePath
 
-hashClick = Signal.mailbox Nothing
+hashClick =
+  Signal.mailbox Nothing
 
 port set : Signal (Task.Task String ())
 port set =
@@ -37,21 +59,3 @@ port set =
                 Just y -> setPath y
                 Nothing -> Task.fail "")  
   hashClick.signal
-
-routing (pagePath, sig1, sig2) =
-  let sig1Page = always sig1
-      sig2Page = always sig2
-      allPage =
-        match [ "" :-> sig1Page
-              , "#/Signal1" :-> sig1Page
-              , "#/Signal2" :-> sig2Page
-        ] sig1Page
-  in
-    allPage pagePath
-
-routingElement = Signal.map routing signals
-
-main =
-  Extra.mapMany (flow left) [
-    routingElement
-  ]
