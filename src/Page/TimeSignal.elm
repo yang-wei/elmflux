9b535103de006c6e23a642e93f01317c11210851
yang-wei <yangweilim21@gmail.com>
2016-02-15 10:14:34 +0900

    Add time note

--- src/Page/TimeSignal.elm
+++ src/Page/TimeSignal.elm
@@ -15,14 +15,68 @@ import Component.Note exposing (signalNote)
 app : Signal Element
 app =
   Extra.mapMany (flow down)
-    [ timeSignal
-    , timeSignalNote
+    [ fpsSignalNote
+    , fpsSignal
+    , fpsWhenSignalNote
+    , isMouseDownSignal
+    , fpsWhenSignal
+    , timeEverySignalNote
+    , timeEveryNote
+    , timeDelaySignalNote
+    , mouseClicksSignal
+    , timeDelaySignal
     ]
 
-timeSignal : Signal Element
-timeSignal =
+
+-- Time.fps
+
+fpsSignalNote : Signal Element
+fpsSignalNote =
+  signalNote "number -> Signal Time"
+
+fpsSignal : Signal Element
+fpsSignal =
+  displaySimpleSandbox [ ( (Time.fps 1), "Time.fps 1") ]
+
+
+-- Time.fpsWhen
+
+fpsWhenSignalNote : Signal Element
+fpsWhenSignalNote =
+  signalNote "number -> Signal Bool -> Signal Time"
+
+isMouseDown = Mouse.isDown
+
+isMouseDownSignal : Signal Element
+isMouseDownSignal =
+  displaySimpleSandbox [ ( isMouseDown, "isMouseDown = Mouse.isDown") ]
+
+fpsWhenSignal : Signal Element
+fpsWhenSignal =
+  displaySimpleSandbox [ ( Time.fpsWhen 1 isMouseDown, "Time.fpsWhen 1 isMouseDown") ]
+
+
+-- Time.every
+
+timeEverySignalNote : Signal Element
+timeEverySignalNote =
+  signalNote "Time -> Signal Time"
+
+timeEveryNote : Signal Element
+timeEveryNote =
   displaySimpleSandbox [ ( (Time.every Time.second), "Time.every Time.second") ]
 
-timeSignalNote : Signal Element
-timeSignalNote =
-  signalNote "The clock is ticking... Showing current time in unix timestamp."
+
+-- Time.delay
+
+timeDelaySignalNote : Signal Element
+timeDelaySignalNote =
+  signalNote "Time -> Signal a -> Signal a"
+
+mouseClicksSignal : Signal Element
+mouseClicksSignal =
+  displaySimpleSandbox [ ( Mouse.clicks, "Mouse.clicks" ) ]
+
+timeDelaySignal : Signal Element
+timeDelaySignal =
+  displaySimpleSandbox [ ( Time.delay (Time.second) Mouse.clicks, "Time.delay (Time.second) Mouse.clicks" ) ]
