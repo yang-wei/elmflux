43ea9f54c7b01f5b21e2dd26fe398f0903d5d66d
yang-wei <yangweilim21@gmail.com>
2016-02-18 01:43:13 +0900

    Auto deployment all the thing !!!

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+# cleaning up
+rm -rf out || exit 0;
+
+# install elm and its dependencis
+npm --version
+node -version
+
+npm install -g elm
+elm package install --yes
+
+# build
+elm make src/Main.elm --output build/main.js
+
+# create directory
+mkdir -p out
+cp index.html out
+cp -r assets/ out
+cp -r build/ out
+cd out
+
+# ready to deploy to gh-pages branch
+git init
+git config user.name "Travis-CI"
+git config user.email "travis@yang-wei.com"
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
