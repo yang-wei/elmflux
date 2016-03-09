5aabb857bf937004c88b97060f2c77eb30b140b2
yang-wei <yangweilim21@gmail.com>
2016-03-09 17:40:09 +0800

    Color type annotation

--- src/Config/Color.elm
+++ src/Config/Color.elm
@@ -1,9 +1,11 @@
 module Config.Color(elmBlue, white) where
 
-import Color exposing (rgb)
+import Color exposing (rgb, Color)
 
+elmBlue : Color
 elmBlue =
   rgb 96 181 204
 
+white : Color
 white =
   rgb 255 255 255
